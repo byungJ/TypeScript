77ccbf7014e40b5b05d8ad0aab87f299a4426805
김병준 <bppy@gimbyeongjun-ui-MacBookPro-2.local>
2022-05-10 23:25:30 +0900

    get_set

--- 3-OOP/3-3-encapsulation.ts
+++ 3-OOP/3-3-encapsulation.ts
@@ -12,7 +12,7 @@
         private coffeeBeans: number = 0; // instance (object) level
 
         // object를 만들 때 항상 호출 됩니다.
-        constructor(coffeeBeans: number) {
+        private constructor(coffeeBeans: number) {
             this.coffeeBeans = coffeeBeans;
         }
 
@@ -40,7 +40,37 @@
         }
     }
 
-    const maker = new CoffeeMaker(32);
+    // const maker = new CoffeeMaker(32);
+    const maker = CoffeeMaker.makeMachine(32);
     console.log(maker);
     console.log(CoffeeMaker.makeMachine(3));
+
+    class User {
+        get fullName(): string {
+           return `${this.firstName} ${this.lastName}`;
+        }
+
+        private internalAge = 4;
+        get age(): number {
+            return this.internalAge;
+        }
+
+        set age(num:number) {
+            if(num < 0) {
+                throw new Error('유후~~ 0보다 작을 수는 없다구...');
+            }
+            this.internalAge = num;
+        }
+
+        // 변수를 위에서 생서하지 않고 바로 생성
+        constructor(private firstName: string, private lastName: string) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+    const user = new User('Steve', 'Jobs');
+    user.age = 10;
+
+    // get은 함수 같이 생겼지만 접근 할 때는 맴버변수에 접근 하는 것 처럼 접근!!
+    console.log(user.fullName);
 }
