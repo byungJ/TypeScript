3feb13359d2c155c6d3d21a137ea2a9ace3721b4
byungJ <qudwns6122@naver.com>
2022-05-15 22:16:18 +0900

    error

---
+++ 8-EXCEPTION/8-1-exception.ts
@@ -0,0 +1,28 @@
+// Java: Exception
+// JavaScript: Error
+
+// const array = new Array(10000000000000000000000000000000);
+
+// Error(Exception) Handling: try -> catch -> finally
+
+function readFile(fileName: string): string {
+    if(fileName === 'not exist!💩') {
+        throw new Error(`file not exist! ${fileName}`);
+    }
+    return `file contents🗒`;
+}
+
+function closeFile(fileName: string) {
+    //
+}
+
+const fileName = 'file';
+//const fileName = 'not exist!💩';
+try {
+    console.log(readFile(fileName));
+}catch(error) {
+    console.log('catched!!!');
+}finally {
+    closeFile(fileName);
+    console.log('finally!!');
+}
---
+++ 8-EXCEPTION/8-3-state.ts
@@ -0,0 +1,46 @@
+{
+    class NetWorkErrorStateClass extends Error {
+
+    }
+
+    type NewWorkErrorState = {
+        result: 'fail';
+        reason: 'offline' | 'down' | 'timeout';
+    };
+
+    type SuccessState = {
+        result: 'success';
+    };
+
+    type ResultState = SuccessState | NewWorkErrorState;
+    class NetWorkClient {
+        tryConnect(): ResultState {}
+    }
+    
+    class UserService {
+        constructor(private client: NetWorkClient) {}
+    
+        login() {
+            this.client.tryConnect();
+            // login.....
+        }
+    }
+    
+    class App {
+        constructor(private userService: UserService) {}
+        run() {
+            try
+            {
+                this.userService.login();
+            } catch(error) {
+                // 에러는 처리하고 해결 할 수 있는 곳에서 잡는것이 좋습니다.
+                // show dialog to user
+            }
+        }
+    }
+    
+    const client = new NetWorkClient();
+    const service = new UserService(client);
+    const app = new App(service);
+    app.run();
+}
