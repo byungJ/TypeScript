213c65c86dd2a04ba13328b46f847b83ef110dec
byungJ <qudwns6122@naver.com>
2022-07-05 18:01:32 +0900

    motion_1

---
+++ motion/src/components/page/item/image.ts
@@ -0,0 +1,24 @@
+import { BaseComponent } from "../../component.js";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
+    constructor(title: string, url: string) {
+        super(`template.innerHTML =
+        <section class="image">
+            <div class="image__holder">
+                <img class="image__thumbnail">
+            </div>
+            <h2 class="image__title"></h2>
+        </section>`);
+
+        const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement; 
+        imageElement.src = url;
+        imageElement.alt = title;
+
+        const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+        titleElement.textContent = title;
+    }
+
+    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+        parent.insertAdjacentElement(position, this.element);
+    }
+}
