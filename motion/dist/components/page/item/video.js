c3bf260ddb5134544ee17d8e9353288e2a78800a
김병준 <bppy@ttugttag-iMacBook.local>
2022-07-08 00:03:54 +0900

    youtube url

--- motion/dist/components/page/item/video.js
+++ motion/dist/components/page/item/video.js
@@ -1,1 +1,18 @@
-"use strict";
+import { BaseComponent } from "../../component.js";
+export class VideoComponent extends BaseComponent {
+    constructor(title, url) {
+        super(`<section>
+        <div class="video__player">
+            <iframe class="video__iframe">
+    
+            </iframe>
+        </div>
+        <h3 class="video__title"></h3>
+    </section>`);
+        const iframe = this.element.querySelector('.video__iframe');
+        iframe.src = 'https://youtu.be/uQfUPEMVvOY'; // url -> videoId -> embed
+        console.log(url);
+        const titleElement = this.element.querySelector('.video__title');
+        titleElement.textContent = title;
+    }
+}
